f95ba96de06b6e9f8eaca220bdc279d0867409cc
nguyennhat210297 <nguyennhat210297@users.noreply.github.com>
2020-10-26 16:49:02 +0700

    lifecycle

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -6,16 +6,16 @@ import './App.css';
 
 class App extends Component{
 
-  constructor(){ // constructor là phương thức được gọi tự động bởi 'new" chúng ta có thể khởi tạo đối tượng ở đó
-    super(); // super.method() để gọi một phương thức cha, super(); để gọi một parent constructor, chỉ bên trong contructor
+  constructor(props){ // constructor là phương thức được gọi tự động bởi 'new" chúng ta có thể khởi tạo đối tượng ở đó
+    super(props); // super.method() để gọi một phương thức cha, super(); để gọi một parent constructor, chỉ bên trong contructor
 
     this.state = { // chứa nhiều loại thuộc tính.
       monsters: [],
       // set gia tri cua tim kiem = trong de tra ve toan bo du lieu
-      searchField1: ''
+      searchField1: '',
+      somacdinh: 12,
     };
     this.handleChange = this.handleChange.bind(this);
-    //this.sukienclick2 = this.sukienclick.bind(this);
   }
   // Life Cycle Methods
   componentDidMount(){
@@ -28,18 +28,26 @@ class App extends Component{
     this.setState({searchField1: e.target.value});
     //TypeError: Unable to get property 'setState' of undefined or null reference
   }
-  // sukienclick(){
-  //   console.log(this)
-  // }
-  // sukienclick2 = () => console.log('click2');
+  handleSo = () =>{
+    this.setState((preState, preProps) =>{
+      return {somacdinh: preState.somacdinh + preProps.increment}
+    }, (e) => {});
+    console.log(this.state.somacdinh)
+  }
   render(){
-    const { monsters, searchField1 } = this.state;
+    const { monsters, searchField1, title } = this.state;
     //fillter Phuong pháp tìm kiếm phần tử đơn lẻ và trả về true
     const filteredMonsters = monsters.filter(monte =>
       monte.name.toLowerCase().includes(searchField1.toLowerCase()));
     return (
       <div className="App">
-        <h1> Trang chu </h1>
+        <p>{this.state.somacdinh}</p>
+        <button
+          onClick={this.handleSo}
+        >
+          Thay đổi
+        </button>
+        <h1>Trang Chủ</h1>
         <SearchBox
           placeholder='Tìm kiếm!'
           handleChange={this.handleChange}
